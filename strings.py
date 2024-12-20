8ce88d9cd83faf5d417093de7c991aaeb681e244
valover33 <83272190+valover33@users.noreply.github.com>
2024-12-20 12:32:29 +0300

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,8 +1,6 @@
 #   Практическое задание по уроку "Строки и индексация строк"
-from tkinter.scrolledtext import example
 
 example = "Топинамбур"
-
 print(example[0])
 print(example[-1])
 print(example[5:])
