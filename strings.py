6fe1733300be2d9c8b2dbb5d361df67bc79f79fd
valover33 <83272190+valover33@users.noreply.github.com>
2024-12-20 12:31:16 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,11 @@
+#   Практическое задание по уроку "Строки и индексация строк"
+from tkinter.scrolledtext import example
+
+example = "Топинамбур"
+
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1::2])
+
